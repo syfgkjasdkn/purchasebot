c34c456ba8f61b5bce1503fe928ba62892093433
ark <a@s>
2018-11-06 14:19:14 +0000

    make webhook configurable

--- apps/tgbot/lib/tgbot/application.ex
+++ apps/tgbot/lib/tgbot/application.ex
@@ -4,6 +4,7 @@ defmodule TGBot.Application do
 
   def start(_type, _args) do
     load_tg_token!()
+    set_webhook!()
   end
 
   @doc false
@@ -11,5 +12,11 @@ defmodule TGBot.Application do
     token = System.get_env("TG_TOKEN") || raise(ArgumentError, "need TG_TOKEN to be set")
     Application.put_env(:tgbot, :token, token)
     Application.put_env(:nadia, :token, token)
+  end
+
+  @doc false
+  def set_webhook! do
+    webhook = System.get_env("WEBHOOK") || raise(ArgumentError, "need WEBHOOK to be set")
+    {:ok, _} = TGBot.set_webhook(webhook)
   end
 end
