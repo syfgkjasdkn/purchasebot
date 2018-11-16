3e912c852c89b78344f913a63fc7ebe550a17277
ark <a@s>
2018-11-16 13:20:53 +0000

    add a sanity check test

--- apps/tgbot/test/tgbot_test.exs
+++ apps/tgbot/test/tgbot_test.exs
@@ -1,3 +1,11 @@
 defmodule TGBotTest do
   use ExUnit.Case
+
+  test "set webhook" do
+    TGBot.set_webhook(url: "https://some.website/tgbot", certificate: "priv/somekey.pem")
+
+    assert_receive {:webhook,
+                    url: "https://some.website/tgbot/1263745172:iugyaksdfhjfgrgyuwekfhjsdb",
+                    certificate: "priv/somekey.pem"}
+  end
 end
